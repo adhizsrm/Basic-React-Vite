171e7590c22cc17e7456508032a5500c6dcae7e0
adhizsrm <adhizsrm@gmail.com>
2025-04-27 11:48:19 +0530

    adding seperate images folder, and adding background image to MainComponent

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,7 @@ export default function Navbar() {
     return (
         <header>
             <nav>
-                <img src="./src/assets/react-logo.png" className="react-logo" alt="React Logo"/>
+                <img src="../images/react-logo.png" className="react-logo" alt="React Logo"/>
                 <span>ReactFacts</span>
             </nav>
         </header>
