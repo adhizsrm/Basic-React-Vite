e4e4779fa11901a39fadc556a59d3aad03a472db
adhizsrm <adhizsrm@gmail.com>
2025-04-27 11:10:48 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Navbar from "/src/components/Navbar.jsx";
+import MainComponent from "/src/components/MainComponent.jsx";
+export default function App() {
+  return (
+    <>
+      <Navbar />
+      <MainComponent/>
+    </>
+  );
+}
